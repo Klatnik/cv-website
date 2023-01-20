14ee53974d17ef0e22640dab2fdb64e2fcd4aafb
Kateřina Holíková <holikka@gmail.com>
2023-01-20 19:02:08 +0100

    změny v carousel

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
-(function(){
+const slider = document.querySelector(".items");
+		const slides = document.querySelectorAll(".item");
+		const button = document.querySelectorAll(".button");
 
-    "use strict";
+		let current = 0;
+		let prev = 4;
+		let next = 1;
 
-    var cvs,ctx;
-    var nodes = 6;
-    var waves = [];
-    var waveHeight = 60;
-    var colours = ["#f80000","#00f800","#0000f8"];
+		for (let i = 0; i < button.length; i++) {
+			button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+		}
 
+		const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
 
+		const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
 
-// Initiator function
+		const gotoNum = number => {
+			current = number;
+			prev = current - 1;
+			next = current + 1;
 
-function init() {
+			for (let i = 0; i < slides.length; i++) {
+				slides[i].classList.remove("active");
+				slides[i].classList.remove("prev");
+				slides[i].classList.remove("next");
+			}
 
-cvs = document.getElementById("canvas");
+			if (next == 5) {
+				next = 0;
+			}
 
-ctx = cvs.getContext("2d");
+			if (prev == -1) {
+				prev = 4;
+			}
 
-resizeCanvas(cvs);
-
-
-
-for (var i = 0; i < 3; i++) {
-
-  waves.push(new waves(colours[i], 1, nodes));
-
-}
-update();
-
-}
-function update() {
-
-var fill = window.getComputedStyle(document.querySelector("header"),null).getPropertyValue("background-color"); 
-
+			slides[current].classList.add("active");
+			slides[prev].classList.add("prev");
+			slides[next].classList.add("next");
+		}
